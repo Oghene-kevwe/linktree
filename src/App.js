5297774f018f28ff69ebfe418a3ba5d86fe7a816
Idise Praise <Idisepraise@gmail.com>
2022-11-04 03:33:38 +0100

    fix:move components to pages folder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { Profile } from "./profile";
-import { Link } from "./link";
+import { Profile } from "./pages/profile";
+import { Link } from "./pages/link";
 import { SocialIcons } from "./social-icons";
-import { Footer } from "./footer";
+import { Footer } from "./pages/footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // CSS
 import "./index.css";
 
 export const App = () => {
   return (
-    <main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="link" element={<Link />} />
+        <Route path=" " element={<SocialIcons />} />
+        <Route path="/" element={<Footer />} />
+        {/* <main>
       <Profile />
       <Link />
       <SocialIcons />
       <Footer/>
-    </main>
+    </main> */}
+      </Routes>
+    </BrowserRouter>
   );
 };
