9ff11c60805ec8f161767e979aa014c8e6b5d09f
Idise Praise <Idisepraise@gmail.com>
2022-11-05 13:49:40 +0100

    refactor: refactor display of links links.js

--- src/components/links.js
+++ src/components/links.js
@@ -1,64 +1,62 @@
 import { Link } from "react-router-dom";
 
 export const Links = () => {
+  const data = [
+    {
+      id: "twitter",
+      link: "https://twitter.com/PraiseIdise",
+      name: "Twitter link",
+    },
+    { id: "btn__zuri", link: "https://training.zuri.team/", name: "zuri team" },
+    {
+      id: "books",
+      link: "http://books.zuri.team/",
+      name: "zuri books",
+      title: "this is where you find books about design and coding",
+    },
+    {
+      id: "book__python",
+      link: "https://books.zuri.team/python-for-beginners?ref_id=<idise praise>",
+      title:
+        "The HNG Python Book gives you the tips & guidelines you need to help you grow and advance your tech careers",
+      name: " python books",
+    },
+    {
+      id: "pitch",
+      link: "https://background.zuri.team/",
+      title:
+        "Skill2Hire is an online platform that allows you to run background checks on coders and hire a freelancer with the necessary skill set for your job",
+      name: " background check for coders",
+    },
+    {
+      id: "book_design",
+      link: "https://books.zuri.team/design-rules",
+      title:
+        "The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified",
+      name: "design books",
+    },
+  ];
+
   return (
     <section className="links-container">
-      {/* twitter */}
-      <a href="https://twitter.com/PraiseIdise" className="link-btn" 
-        target="_blank">
-        <span>Twitter link</span>
-      </a>
-      {/* zuri team */}
-      <a
-        href="https://training.zuri.team/"
-        className="link-btn"
-        id="btn__zuri"
-        target="_blank"
-      >
-        zuri team
-      </a>
-      {/* zuri books */}
-      <a
-        href="http://books.zuri.team/"
-        className="link-btn"
-        id="books"
-        target="_blank"
-        title="this is where you find books about design and coding"
-      >
-        zuri books
-      </a>
-      {/* python books */}
-      <a
-        href="https://books.zuri.team/python-for-beginners?ref_id=<idise praise> "
-        className="link-btn"
-        id="book__python"
-        title="The HNG Python Book gives you the tips & guidelines you need to help you grow and advance your tech careers"
-        target="_blank"
-      >
-        python books
-      </a>
-      {/* background check */}
-      <a
-        href="https://background.zuri.team/"
-        className="link-btn"
-        id="pitch"
-        title="Skill2Hire is an online platform that allows you to run background checks on coders and hire a freelancer with the necessary skill set for your job"
-        target="_blank"
-      >
-        background check for coders
-      </a>
-      {/* book design */}
-      <a
-        href="https://books.zuri.team/design-rules"
-        className="link-btn"
-        id="book_design"
-        title="The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified"
-        target="_blank"
-      >
-        design books
-      </a>
+      {data.map(({ id, link, title, name }) => {
+        return (
+          <a
+            href={link}
+            className="link-btn"
+            id={id}
+            target="_blank"
+            title={title}
+          >
+            {name}
+          </a>
+        );
+      })}
+
       {/* contact */}
-      <Link to='/contact'  className="link-btn" id="contact">contact</Link>
+      <Link to="/contact" className="link-btn" id="contact">
+        contact
+      </Link>
     </section>
   );
 };
